15a9aa2cb5a998ad8ad8f7387d2fdf8018fde12c
Fatima Azzahro <fatima@paypro.id>
2018-03-08 16:02:34 +0700

    [Exercise 3] Click and Saving State in Fragments - Set onClick Listener for all Fragments

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -55,7 +55,7 @@ public class BodyPartFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
         // Get a reference to the ImageView in the fragment layout
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
         // If a list of image ids exists, set the image resource to the correct item in that list
         // Otherwise, create a Log statement that indicates that the list was not found
@@ -63,8 +63,22 @@ public class BodyPartFragment extends Fragment {
             // Set the image resource to the list item at the stored index
             imageView.setImageResource(mImageIds.get(mListIndex));
 
-            // TODO (1) Set a click listener on the image view and on a click increment the list index and set the image resource
-            // TODO (2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
+            // COMPLETED (1) Set a click listener on the image view and on a click increment the list index and set the image resource
+            // COMPLETED (2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Increment position as long as the index remains <= the size of the image ids list
+                    if (mListIndex < mImageIds.size()-1) {
+                        mListIndex++;
+                    } else {
+                        // The end of list has been reached, so return to beginning index
+                        mListIndex = 0;
+                    }
+                    // Set the image resource to the new list item
+                    imageView.setImageResource(mImageIds.get(mListIndex));
+                }
+            });
 
         } else {
             Log.v(TAG, "This fragment has a null list of image id's");
