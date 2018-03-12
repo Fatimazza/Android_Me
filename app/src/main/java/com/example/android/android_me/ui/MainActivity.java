b1d46ac3bc2e475295504670fd8b8252724d219e
Fatima Azzahro <fatima@paypro.id>
2018-03-12 14:48:43 +0700

    [Exercise 6] Communicate Between Activities - Launch Intent to other Activity when next Button is clicked

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -19,6 +19,8 @@ package com.example.android.android_me.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -79,7 +81,16 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         final Intent intent = new Intent(this, AndroidMeActivity.class);
         intent.putExtras(b);
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+
+        // COMPLETED (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        // The "Next" button launches a new AndroidMeActivity
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
 
     }
 
